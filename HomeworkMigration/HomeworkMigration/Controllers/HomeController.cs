b3ef93478696377a7f4fa288ab7af594824d9a26
Said Mikailov <saidgm@code.edu.az>
2022-03-13 19:49:00 +0400

    Homework

---
+++ HomeworkMigration/HomeworkMigration/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HomeworkMigration.Data;
+using HomeworkMigration.Models;
+using HomeworkMigration.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeworkMigration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+
+            ValentineContent valentineContent = await _context.ValentineContents.FirstOrDefaultAsync();
+
+            HomeVM homeVM = new HomeVM()
+            {
+                Sliders = sliders,
+                ValentineContent = valentineContent
+            };
+            
+            return View(homeVM);
+        }
+
+        
+    }
+}
---
+++ HomeworkMigration/HomeworkMigration/ViewModels/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using HomeworkMigration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeworkMigration.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+        public ValentineContent ValentineContent { get; set; }
+    }
+}
